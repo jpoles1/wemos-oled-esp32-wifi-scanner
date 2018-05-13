60df8b45bc4d21c68174ff45e7b806ce1ed66dd4
Jordan Poles <jpoles1@gmail.com>
2018-05-12 22:39:07 -0400

    Wifi scanner is functional!

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,90 @@
 #include "WiFi.h"
-#include <esp_wifi.h>
 
 #include <Wire.h>
 #include "SSD1306.h"
 #include <bits/stdc++.h>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 SSD1306  display(0x3c, 5, 4);
 
-void setup(){
-    display.init();
-    // display.flipScreenVertically();
-    display.setContrast(255);
-    display.setLogBuffer(5, 30);
-
-    Serial.begin(115200);
-
-    // Set WiFi to station mode and disconnect from an AP if it was previously connected
-    WiFi.mode(WIFI_STA);
-    WiFi.disconnect();
-    delay(100);
-
-    Serial.println("Setup done");
+std::vector <std::string> splitString(std::string input, unsigned string_size = 10){
+  std::vector <std::string> split_string;
+  for (unsigned i = 0; i < input.length(); i += string_size) {
+    split_string.push_back(input.substr(i, string_size));
+  }
+  return split_string;
 }
-void displayString(std::string display_data, bool clearScreen = false){
+
+void displayString(std::string display_data, bool newline = true, bool clearScreen = false){
   if(clearScreen == true){
     display.setLogBuffer(5, 30);
   }
-  display.clear();
+  std::vector <std::string> split_string = splitString(display_data, 21);
+  for (std::vector<std::string>::iterator it = split_string.begin() ; it != split_string.end(); ++it){ 
+      display.clear();
+      std::string txt = *it;
+      char *cstr = new char[txt.length() + 1];
+      strcpy(cstr, txt.c_str());
+      if(newline){
+        Serial.println(cstr);
+        display.println(cstr);
+      } else {
+        Serial.print(cstr);
+        display.print(cstr);
+      }
+      delete [] cstr;
+      // Draw it to the internal screen buffer
+      display.drawLogBuffer(0, 0);
+      // Display it on the screen
+      display.display();
+  }
   // Print to the screen
-  char *cstr = new char[display_data.length() + 1];
-  strcpy(cstr, display_data.c_str());
-  Serial.println(cstr);
-  display.println(cstr);
-  delete [] cstr;
-  // Draw it to the internal screen buffer
-  display.drawLogBuffer(0, 0);
-  // Display it on the screen
-  display.display();
-  delay(250);
+  if(newline){
+    delay(500);
+  }
 }
+
+void displayInt(int display_data, bool newline = false, bool clearScreen = false){
+  std::ostringstream ss;
+  ss.str("");
+  ss << display_data;
+  displayString(ss.str(), newline, clearScreen);
+}
+
 void displayStringVector(std::vector <std::string> display_data, bool clearScreen = false){
   if(clearScreen == true){
     display.setLogBuffer(5, 30);
   }
   for (std::vector<std::string>::iterator it = display_data.begin() ; it != display_data.end(); ++it){
-    display.clear();
     // Print to the screen
-    std::string str = *it;
-    char *cstr = new char[str.length() + 1];
-    strcpy(cstr, str.c_str());
-    Serial.println(cstr);
-    display.println(cstr);
-    delete [] cstr;
-    // Draw it to the internal screen buffer
-    display.drawLogBuffer(0, 0);
-    // Display it on the screen
-    display.display();
-    delay(500);
+    displayString(*it, true, clearScreen);
   }
 }
+
+void setup(){
+    Serial.begin(115200);
+    // Set WiFi to station mode and disconnect from an AP if it was previously connected
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+
+    display.init();
+    // display.flipScreenVertically();
+    display.setContrast(255);
+    display.setLogBuffer(5, 30);
+
+    displayString("Setup complete");
+}
+
 void loop()
 {
-    displayString("Scan started...", true);
+    displayString("WiFi scan started...", true, true);
 
     // WiFi.scanNetworks will return the number of networks found
     int num_aps = WiFi.scanNetworks();
+    delay(500);
     displayString("Scan complete!");
     displayString("--------------------");
     if (num_aps == 0) {
@@ -75,19 +93,14 @@ void loop()
         std::ostringstream ss;
         ss << num_aps << " networks found:";
         displayString(ss.str());
-        ss.str("");
-        ss << WiFi.SSID(0);
-        displayString(ss.str());
         std::vector <std::string> display_data;
         for (int i = 0; i < num_aps; i++) {
-          String ssid = WiFi.SSID(i);
-          std::ostringstream stringStream;
-          stringStream.str("");
-          stringStream << i+1 << ": ";
-          stringStream << ssid << " ";
-          stringStream << "(" << WiFi.RSSI(i) << " db)" ;
-          stringStream << (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*";
-          display_data.push_back(stringStream.str());
+          ss.str("");
+          ss << i+1 << ": ";
+          ss << WiFi.SSID(i).c_str() << " ";
+          ss << "(" << WiFi.RSSI(i) << " db)" ;
+          ss << String ((WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*").c_str();
+          display_data.push_back(ss.str());
         }
         displayStringVector(display_data);
     }
